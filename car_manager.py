5ea5bfb352a94209db524cf973b8c98f08e69bb0
kortlon <kortlonerskine@me.com>
2023-07-27 01:38:21 -0500

    Finished car generation

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,40 @@
+import random
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
+class CarManager():
+    # def __init__(self):
+    #     super().__init__()
+    #     self.penup()
+    #     self.color(COLORS[random.randint(0,5)])
+    #     self.sety(random.randint(-250,250))
+    #     self.setx(280)
+    #     self.shape('square')
+    #     self.width(40)
+    #     self.shapesize(1,2)
+    #     self.setheading(180)
+    #     self.forward(STARTING_MOVE_DISTANCE)
+    #
+    # def move(self):
+    #     while self.xcor() > -300:
+    #         self.forward(MOVE_INCREMENT)
+    def __init__(self):
+        self.cars = []
 
-class CarManager:
+    def create_car(self):
+        new_car = Turtle("square")
+        new_car.penup()
+        new_car.color(COLORS[random.randint(0,5)])
+        new_car.sety(random.randint(-250,250))
+        new_car.setx(280)
+        new_car.shapesize(1,2)
+        new_car.setheading(180)
+        self.cars.append(new_car)
+
+    def move(self):
+        for number_cars in range (0, len(self.cars)):
+            self.cars[number_cars].forward(MOVE_INCREMENT)
     pass
--- main.py
+++ main.py
@@ -3,12 +3,26 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
-
+car_manager = CarManager()
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Tutle Crossing")
 screen.tracer(0)
+screen.listen()
+player = Player()
 
+
+screen.onkey(player.move_forward, "Up")
+
+counter = 0
 game_is_on = True
 while game_is_on:
+    counter += 1
+    if counter == 6:
+        counter = 0
+        car_manager.create_car()
+    car_manager.move()
+
+
     time.sleep(0.1)
     screen.update()
